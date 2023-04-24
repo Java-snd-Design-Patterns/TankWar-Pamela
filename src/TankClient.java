9e7c85288138881366472a947ed868e746d83b95
user <pamelaputri96@gmail.com>
2023-04-24 23:32:02 +0800

    Test version 0.2 of Tank War

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,10 +1,18 @@
 import java.awt.*;
+import java.awt.event.*;
 
 public class TankClient extends Frame {
 
 	public void launchFrame() {
-		this.setLocation(300, 100);
+		this.setLocation(300, 50);
 		this.setSize(800, 600);
+		this.setTitle("TankWar");
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		setResizable(false);
 		setVisible(true);
 	}
 
