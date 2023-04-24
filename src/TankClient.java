eed7a8c2da50e2c09648571b57b08c519445bc9d
user <pamelaputri96@gmail.com>
2023-04-24 13:45:10 +0800

    Test version 0.1 of Tank War

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,16 +1,9 @@
-import java.awt.*;
 
-public class TankClient extends Frame {
-
-	public void launchFrame() {
-		this.setLocation(300, 100);
-		this.setSize(800, 600);
-		setVisible(true);
-	}
+public class TankClient {
 
 	public static void main(String[] args) {
-		TankClient tc = new TankClient();
-		tc.launchFrame();
+		// TODO Auto-generated method stub
 
 	}
+
 }
