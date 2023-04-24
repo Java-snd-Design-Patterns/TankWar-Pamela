86aa5c0096313300985fb3a0b9e0fcc0227a09a1
user <pamelaputri96@gmail.com>
2023-04-24 23:36:05 +0800

    Test version 0.3 of Tank War

--- src/TankClient.java
+++ src/TankClient.java
@@ -3,7 +3,16 @@ import java.awt.event.*;
 
 public class TankClient extends Frame {
 
+//The paint method does not need to be called and will be automatically called once it is to be redrawn
+	public void paint(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(50, 50, 30, 30);
+		g.setColor(c);
+	}
+
 	public void launchFrame() {
+
 		this.setLocation(300, 50);
 		this.setSize(800, 600);
 		this.setTitle("TankWar");
@@ -12,7 +21,8 @@ public class TankClient extends Frame {
 				System.exit(0);
 			}
 		});
-		setResizable(false);
+		this.setResizable(false);
+		this.setBackground(Color.GREEN);
 		setVisible(true);
 	}
 
