d6386bc6a136f2d4df62d4a31a1ae3f6d3d64e3c
user <user@DESKTOP-9UQJ1LQ>
2023-04-26 20:00:57 +0800

    Test version 1.2 of Tank War

--- src/Tank.java
+++ src/Tank.java
@@ -6,6 +6,11 @@ public class Tank {
 	public static final int XSPEED = 5;
 	public static final int YSPEED = 5;
 
+	public static final int WIDTH = 30;
+	public static final int HEIGHT = 30;
+
+	TankClient tc = null;
+
 	private int x, y;
 
 	private boolean bL = false, bU = false, bR = false, bD = false;
@@ -15,18 +20,53 @@ public class Tank {
 	};
 
 	private Direction dir = Direction.STOP;
+	private Direction ptDir = Direction.D;
 
 	public Tank(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	public Tank(int x, int y, TankClient tc) {
+		this(x, y);
+		this.tc = tc;
+	}
+
 	public void draw(Graphics g) {
 		Color c = g.getColor();
 		g.setColor(Color.RED);
-		g.fillOval(x, y, 30, 30);
-
+		g.fillOval(x, y, WIDTH, HEIGHT);
 		g.setColor(c);
+
+		switch (ptDir) {
+		case L:
+			g.drawLine(x + Tank.WIDTH / 2, y + Tank.HEIGHT / 2, x, y + Tank.HEIGHT / 2);
+			break;
+		case LU:
+			g.drawLine(x + Tank.WIDTH / 2, y + Tank.HEIGHT / 2, x, y);
+			break;
+		case U:
+			g.drawLine(x + Tank.WIDTH / 2, y + Tank.HEIGHT / 2, x + Tank.WIDTH / 2, y);
+			break;
+		case RU:
+			g.drawLine(x + Tank.WIDTH / 2, y + Tank.HEIGHT / 2, x + Tank.WIDTH, y);
+			break;
+		case R:
+
+			g.drawLine(x + Tank.WIDTH / 2, y + Tank.HEIGHT / 2, x + Tank.WIDTH, y + Tank.HEIGHT / 2);
+			break;
+		case RD:
+			g.drawLine(x + Tank.WIDTH / 2, y + Tank.HEIGHT / 2, x + Tank.WIDTH, y + Tank.HEIGHT);
+			break;
+		case D:
+			g.drawLine(x + Tank.WIDTH / 2, y + Tank.HEIGHT / 2, x + Tank.WIDTH / 2, y + Tank.HEIGHT);
+			break;
+		case LD:
+			g.drawLine(x + Tank.WIDTH / 2, y + Tank.HEIGHT / 2, x, y + Tank.HEIGHT);
+			break;
+		case STOP:
+			break;
+		}
 
 		move();
 	}
@@ -45,6 +85,7 @@ public class Tank {
 			break;
 		case RU:
 			x += XSPEED;
+
 			y -= YSPEED;
 			break;
 		case R:
@@ -64,13 +105,18 @@ public class Tank {
 		case STOP:
 			break;
 		}
+		if (this.dir != Direction.STOP) {
+			this.ptDir = this.dir;
+		}
 	}
 
 	public void KeyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		switch (key) {
+		case KeyEvent.VK_CONTROL:
+			tc.m = fire();
+			break;
 		case KeyEvent.VK_LEFT:
-
 			bL = true;
 			break;
 		case KeyEvent.VK_UP:
@@ -81,6 +127,25 @@ public class Tank {
 			break;
 		case KeyEvent.VK_DOWN:
 			bD = true;
+			break;
+		}
+		locateDirection();
+	}
+
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_LEFT:
+			bL = false;
+			break;
+		case KeyEvent.VK_UP:
+			bU = false;
+			break;
+		case KeyEvent.VK_RIGHT:
+			bR = false;
+			break;
+		case KeyEvent.VK_DOWN:
+			bD = false;
 			break;
 		}
 		locateDirection();
@@ -105,7 +170,12 @@ public class Tank {
 			dir = Direction.LD;
 		else if (!bL && !bU && !bR && !bD)
 			dir = Direction.STOP;
-
 	}
 
+	public Missile fire() {
+		int x = this.x + Tank.WIDTH / 2 - Missile.WIDTH / 2;
+		int y = this.y + Tank.HEIGHT / 2 - Missile.WIDTH / 2;
+		Missile m = new Missile(x, y, ptDir);
+		return m;
+	}
 }
